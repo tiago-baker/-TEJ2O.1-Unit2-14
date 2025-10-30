d2fb670b21d0ef503f8f4f9681545f5ba5eb8bdb
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-10-29 22:15:09 -0400

    done

--- main.ts
+++ main.ts
@@ -1,8 +1,110 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Tiago Baker
+ * Created on: Oct 2025
+ * This program makes pixels go around primeter
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounter = 0
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+
+// when A is pressed, the pixels move
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+
+    // top row
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, 0)
+        loopCounter = loopCounter + 1
+        basic.pause(500)
+    }
+
+    // right side
+    loopCounter = 0
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.X, 4)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(500)
+    }
+
+    // bottom
+    loopCounter = 5
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, 4)
+        loopCounter = loopCounter - 1
+        basic.pause(500)
+    }
+
+    // left side
+    loopCounter = 5
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, 0)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(500)
+    }
+
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
+
+// when B is pressed, the pixels move around the outside edge
+input.onButtonPressed(Button.B, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter = 5
+    sprite = game.createSprite(5, 5)
+
+
+    // right side
+    loopCounter = 4
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, 4)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(500)
+    }
+
+    // top row
+    loopCounter = 4
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, 0)
+        loopCounter = loopCounter - 1
+        basic.pause(500)
+    }
+
+    // left side
+    loopCounter = 0
+    while (loopCounter <= 4) {
+        sprite.set(LedSpriteProperty.X, 0)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(500)
+    }
+
+    // botton
+    loopCounter = 0
+    while (loopCounter <= 4) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, 4)
+        loopCounter = loopCounter + 1
+        basic.pause(500)
+    }
+
+
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
